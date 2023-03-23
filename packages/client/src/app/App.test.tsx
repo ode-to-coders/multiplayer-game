8a8ad35eecf8f8e8254921e8eea23eb549edec71
dreamonaut <dreamonautre@gmail.com>
2023-03-23 22:22:19 +0500

    Добавляет роутинг, mui и начальную структуру папок

---
+++ packages/client/src/app/App.test.tsx
@@ -0,0 +1,14 @@
+import App from './App';
+import { render, screen } from '@testing-library/react';
+
+const appContent = 'Main';
+
+// @ts-ignore
+global.fetch = jest.fn(() =>
+  Promise.resolve({ json: () => Promise.resolve('hey') })
+);
+
+test('Example test', async () => {
+  render(<App />);
+  expect(screen.getAllByText(appContent)).toBeDefined();
+});
