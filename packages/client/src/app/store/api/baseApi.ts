e89a66527f6cbefca2210005b5cdcba7818bacef
dreamonaut <dreamonautre@gmail.com>
2023-04-10 21:19:10 +0500

    ODE-33 делает бек для авторизации/регистрации

---
+++ packages/client/src/app/store/api/baseApi.ts
@@ -0,0 +1,13 @@
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+
+const BASE_API_PATH = 'https://ya-praktikum.tech/api/v2/';
+
+const baseQuery = fetchBaseQuery({
+  baseUrl: `${BASE_API_PATH}`,
+});
+
+export const baseApi = createApi({
+  reducerPath: 'baseApi',
+  baseQuery,
+  endpoints: () => ({}),
+});
--- packages/client/src/pages/Signin/index.tsx
+++ packages/client/src/pages/Signin/index.tsx
@@ -1,11 +1,11 @@
-import { AuthForm } from "features/AuthForm";
+import { AuthForm } from 'features/AuthForm';
 
-import s from "./index.module.scss";
+import s from './index.module.scss';
 
 export const Signin = () => {
   return (
     <div className={s.wrapCont}>
       <AuthForm />
     </div>
-  )
-}
+  );
+};
