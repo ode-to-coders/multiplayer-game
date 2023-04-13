9f9e489943b7da93bbe172bfb172eb709cadf561
shinonhorror <shinonhorror@gmail.com>
2023-04-13 18:38:56 +0300

    fix tests

--- packages/client/src/features/ModalEnd/ModalEnd.tsx
+++ packages/client/src/features/ModalEnd/ModalEnd.tsx
@@ -1,7 +1,7 @@
 import avatar from './avatar.png';
 import roomRating from '../../mocks/roomRating.json';
 import { ButtonGroupBase, ModalBase, TableBase } from '../../shared/ui';
-import { reverseSort } from '@/shared/utils/helpers';
+import { reverseSort } from '../../shared/utils/helpers';
 
 const tableNames = ['Место', 'Никнейм', 'Набрано очков'];
 
