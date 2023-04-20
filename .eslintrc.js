9b6874334b1fa65ab68a7cf4bf62ec654b845d7b
aleksakko <aleksakko@yandex.ru>
2023-04-20 15:01:19 +0300

    fix lint-error

--- .eslintrc.js
+++ .eslintrc.js
@@ -15,7 +15,7 @@ module.exports = {
   },
   plugins: ['@typescript-eslint'],
   rules: {
-    '@typescript-eslint/ban-ts-comment': 1,
+    '@typescript-eslint/ban-ts-comment': 0,
     'quotes': [2, 'single', { 'avoidEscape': true }]
   },
 };
