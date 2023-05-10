632afb5214deede0cd5ae5933c7e2f85dacb2d0c
shinonhorror <shinonhorror@gmail.com>
2023-05-10 14:17:51 +0300

    Fix server

--- packages/server/index.ts
+++ packages/server/index.ts
@@ -9,21 +9,9 @@ dotenv.config();
 import express from 'express';
 import * as fs from 'fs';
 import * as path from 'path';
-import { createClientAndConnect } from './db';
+// import { createClientAndConnect } from './db';
 
-const app = express();
-app.use(cors());
-const port = Number(process.env.SERVER_PORT) || 3001;
-
-createClientAndConnect();
-
-app.get('/', (_, res) => {
-  res.json('👋 Howdy from the server :)');
-});
-
-app.listen(port, () => {
-  console.log(`  ➜ 🎸 Server is listening on port: ${port}`);
-});
+// createClientAndConnect();
 
 type payloadType = {
   success?: boolean;
