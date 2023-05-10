a6e1e0c9632a3bbfa8929868b6f2be1743b35b97
shinonhorror <shinonhorror@gmail.com>
2023-05-10 12:48:18 +0300

    Formatting

--- packages/server/index.ts
+++ packages/server/index.ts
@@ -1,15 +1,25 @@
 import dotenv from 'dotenv';
 import cors from 'cors';
 import WebSocket from 'ws';
-import { createServer as createViteServer } from 'vite';
-import type { ViteDevServer } from 'vite';
 
-//import { createClientAndConnect } from './db';
 dotenv.config();
 
 import express from 'express';
-import * as fs from 'fs';
-import * as path from 'path';
+import { createClientAndConnect } from './db';
+
+const app = express();
+app.use(cors());
+const port = Number(process.env.SERVER_PORT) || 3001;
+
+createClientAndConnect();
+
+app.get('/', (_, res) => {
+  res.json('👋 Howdy from the server :)');
+});
+
+app.listen(port, () => {
+  console.log(`  ➜ 🎸 Server is listening on port: ${port}`);
+});
 
 type payloadType = {
   success?: boolean;
@@ -19,31 +29,24 @@ type payloadType = {
   canStart?: boolean;
   count?: number;
 };
-
 type resultType = {
   type: string;
   payload: payloadType;
 };
-
 type requestType = {
   event: string;
   gameId: string;
   payload: payloadType;
 };
-
 type clientType = {
   login: string;
 } & WebSocket;
-
 type gamesType = Record<string, Array<clientType>>;
-
 const games: gamesType = {};
-
 function start() {
   const wss = new WebSocket.Server({ port: 3002 }, () => {
     console.log('WebSocket server started');
   });
-
   wss.on('connection', (wsClient: clientType) => {
     wsClient.on('message', async message => {
       const request = JSON.parse(message.toString());
@@ -60,7 +63,6 @@ function start() {
       broadcast(request);
     });
   });
-
   function initGame(ws: clientType, gameId: string) {
     if (!games[gameId]) {
       games[gameId] = [ws];
@@ -73,12 +75,9 @@ function start() {
       games[gameId] = games[gameId].filter(wsc => wsc.login !== ws.login);
     }
   }
-
   function broadcast(params: requestType) {
     let result: resultType;
-
     const { gameId } = params.payload as requestType;
-
     games[gameId].forEach((client: clientType) => {
       switch (params.event) {
         case 'connect':
@@ -120,82 +119,4 @@ function start() {
     });
   }
 }
-const isDev = () => process.env.NODE_ENV === 'development';
-
-async function startServer() {
-  const app = express();
-  app.use(cors());
-  const port = Number(process.env.SERVER_PORT) || 3001;
-
-  let vite: ViteDevServer | undefined;
-  const distPath = path.dirname(require.resolve('client/dist/index.html'));
-  const srcPath = path.dirname(require.resolve('client'));
-  const ssrClientPath = require.resolve('client/ssr-dist/client.cjs');
-
-  if (isDev()) {
-    vite = await createViteServer({
-      server: { middlewareMode: true },
-      root: srcPath,
-      appType: 'custom',
-    });
-
-    app.use(vite.middlewares);
-  }
-
-  app.get('/api', (_, res) => {
-    res.json('👋 Howdy from the server :)');
-  });
-
-  //createClientAndConnect(); // подключение к БД
-  if (!isDev()) {
-    app.use('/assets', express.static(path.resolve(distPath, 'assets')));
-  }
-
-  app.use('*', async (req, res, next) => {
-    const url = req.originalUrl;
-
-    try {
-      let template: string;
-
-      if (!isDev()) {
-        template = fs.readFileSync(
-          path.resolve(distPath, 'index.html'),
-          'utf-8'
-        );
-      } else {
-        template = fs.readFileSync(
-          path.resolve(srcPath, 'index.html'),
-          'utf-8'
-        );
-        template = await vite!.transformIndexHtml(url, template);
-      }
-
-      let render: (url: string) => Promise<string>;
-
-      if (!isDev()) {
-        render = (await import(ssrClientPath)).render;
-      } else {
-        render = (await vite!.ssrLoadModule(path.resolve(srcPath, 'ssr.tsx')))
-          .render;
-      }
-
-      const appHtml = await render(url);
-
-      const html = template.replace('<!--ssr-outlet-->', appHtml);
-
-      res.status(200).set({ 'Content-Type': 'text/html' }).end(html);
-    } catch (e) {
-      if (isDev()) {
-        vite!.ssrFixStacktrace(e as Error);
-      }
-      next(e);
-    }
-  });
-
-  app.listen(port, () => {
-    console.log(`  ➜ 🎸 Server is listening on port: ${port}`);
-  });
-}
-
-startServer();
 start();
