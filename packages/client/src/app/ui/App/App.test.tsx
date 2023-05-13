deb3cceb6b5b3b54d82e013f30958562340ab73a
dreamonaut <dreamonautre@gmail.com>
2023-05-13 23:30:18 +0500

    ODE-45 авторизация через яндекс

---
+++ packages/client/src/app/ui/App/App.test.tsx
@@ -0,0 +1,24 @@
+import { render, screen } from '@testing-library/react';
+import '@testing-library/jest-dom';
+
+import { LeaderBoardPage } from 'pages/LeaderBoardPage';
+
+import ratings from '../../../mocks/ratings.json';
+
+const сontent = 'Не туда попали';
+
+// @ts-ignore
+global.fetch = jest.fn(() =>
+  Promise.resolve({ json: () => Promise.resolve('hey') })
+);
+
+test('Example test', async () => {
+  const { getByText } = render(<div>Не туда попали</div>);
+  expect(getByText(сontent)).toBeDefined();
+});
+
+test('LeaderBoard render', () => {
+  render(<LeaderBoardPage users={ratings} />);
+  screen.debug();
+  expect(screen.getByText(/Всего игр/i)).toBeInTheDocument();
+});
