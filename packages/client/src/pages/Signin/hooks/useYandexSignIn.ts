8208b38279f9e7923aeacfe55c214ff9eb160b82
lusorich <44974580+lusorich@users.noreply.github.com>
2023-05-18 16:08:55 +0500

    ODE-45 делает авторизацию через яндекс (#33)
    * fix валидации формы паролей
    
    * Исправляет ошибки при билде
    
    * Fix package.json
    
    * save
    
    * Fix package.json
    
    * resolve conflicts
    
    * ODE-45 авторизация через яндекс
    
    * ODE-45 авторизация через яндекс
    
    * ODE-45 авторизация через яндекс
    
    * ODE-45 авторизация через яндекс
    
    * ODE-45 авторизация через яндекс
    
    ---------
    
    Co-authored-by: aleksakko <aleksakko@yandex.ru>
    Co-authored-by: shinonhorror <shinonhorror@gmail.com>

---
+++ packages/client/src/pages/Signin/hooks/useYandexSignIn.ts
@@ -0,0 +1,49 @@
+import { useCallback, useEffect } from 'react';
+import { useSearchParams } from 'react-router-dom';
+
+import {
+  useGetServiceIdQuery,
+  useSignInYandexMutation,
+} from '../../../app/store/api/auth/authApi';
+
+// Пока захардкодил чтобы можно было проверить на рендере 
+// В дальнейшем будут браться с env
+const OAUTH_REDIRECT_API_PATH =
+  'https://oauth.yandex.ru/authorize?response_type=code';
+const APP_PATH = 'https://multiplayer-game-6e3r.onrender.com/';
+
+const getYandexAuthRedirectLink = ({ clientId }: { clientId: string }) =>
+  `${OAUTH_REDIRECT_API_PATH}&client_id=${clientId}&redirect_uri=${APP_PATH}`;
+
+export const useYandexSignIn = () => {
+  const [signInYandex, { isError }] = useSignInYandexMutation();
+  const [searchParams] = useSearchParams();
+
+  const { data } = useGetServiceIdQuery({
+    redirect_uri: encodeURIComponent(APP_PATH),
+  });
+
+  const authCode = searchParams.get('code');
+
+  useEffect(() => {
+    if (authCode && !isError) {
+      signInYandex({
+        code: authCode,
+        redirect_uri: APP_PATH,
+      });
+    }
+  }, [authCode]);
+
+  const yandexRedirect = useCallback(() => {
+    data?.service_id &&
+      window.open(
+        getYandexAuthRedirectLink({ clientId: data?.service_id }),
+        '_self'
+      );
+  }, [data?.service_id]);
+
+  return {
+    yandexRedirect,
+    isError,
+  };
+};
--- packages/client/src/pages/Signin/index.tsx
+++ packages/client/src/pages/Signin/index.tsx
@@ -1,11 +1,19 @@
 import { AuthForm } from '../../features/AuthForm/index';
 
 import s from './index.module.scss';
+import { StyledButton } from '../../shared/ui/Styled';
+import { useYandexSignIn } from './hooks/useYandexSignIn';
+import { CustomNotification } from '../../shared/ui/CustomNotification/CustomNotification';
 
 export const Signin = () => {
+  const { yandexRedirect, isError } = useYandexSignIn();
+
   return (
     <div className={s.wrapCont}>
-      <AuthForm />
+      <AuthForm>
+        <StyledButton onClick={yandexRedirect}>Войти через Яндекс</StyledButton>
+      </AuthForm>
+      <CustomNotification isUninitialized={!isError} isError={isError} />
     </div>
   );
 };
