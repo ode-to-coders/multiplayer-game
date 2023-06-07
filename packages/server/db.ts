e8335db3f2fee73f762aff46b1dedf91fdbcc686
shinonhorror <shinonhorror@gmail.com>
2023-06-07 12:36:56 +0300

    Добавлен pgadmin + настройка docker-compose

--- packages/server/db.ts
+++ packages/server/db.ts
@@ -6,7 +6,7 @@ const { POSTGRES_USER, POSTGRES_PASSWORD, POSTGRES_DB, POSTGRES_PORT } =
   process.env;
 
 const sequelizeOptions: SequelizeOptions = {
-  host: 'localhost',
+  host: 'postgres',
   port: Number(POSTGRES_PORT),
   username: POSTGRES_USER,
   password: POSTGRES_PASSWORD,
