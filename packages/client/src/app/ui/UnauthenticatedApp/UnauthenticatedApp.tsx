f8ec5d59af0e449c18277851931bcf6b48b7fc78
Maria <57846361+shinonhorror@users.noreply.github.com>
2023-06-11 22:53:51 +0400

    Feature/ode 65 + ode-63 (#44)
    * Update AuthenticatedApp.tsx
    
    * Исправляет часть проблем при ouath
    
    * Feature/ode 66 (#42)
    
    * ODE-66: добавляет автообновление счетчика комментариев
    
    * ODE-66: дорабатывает апи форума
    
    * ODE-66: дорабатывает апи форума
    
    * Добавлен pgadmin + настройка docker-compose (#43)
    
    * Тест деплоя в Яндекс облако
    
    * При пуше в ветку prod + убрала лишние переменные
    
    ---------
    
    Co-authored-by: lusorich <44974580+lusorich@users.noreply.github.com>
    Co-authored-by: dreamonaut <dreamonautre@gmail.com>

--- packages/client/src/app/ui/UnauthenticatedApp/UnauthenticatedApp.tsx
+++ packages/client/src/app/ui/UnauthenticatedApp/UnauthenticatedApp.tsx
@@ -1,14 +1,20 @@
 import React from 'react';
-import { Navigate, Route, Routes } from 'react-router-dom';
+import { Navigate, Route, Routes, useSearchParams } from 'react-router-dom';
 
 import { PAGES } from '../../lib/routes.types';
 import { Layout } from '../../layout/Layout';
 import { AUTH_ROUTES } from '../../lib/routes.config';
 
 export const UnauthenticatedApp = () => {
+  const [searchParams] = useSearchParams();
+  const authCode = searchParams.get('code');
+
   return (
     <Routes>
-      <Route path="*" element={<Navigate to={`${PAGES.SIGNIN}`} />} />
+      <Route
+        path="*"
+        element={<Navigate to={`${PAGES.SIGNIN}/?code=${authCode}`} />}
+      />
       {AUTH_ROUTES.map((route, idx) => (
         <Route element={<Layout />} key={idx}>
           <Route
