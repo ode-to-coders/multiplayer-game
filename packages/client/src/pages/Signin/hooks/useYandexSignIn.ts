328ff9402df6273483e06cdb4ab69af5b8d4f3b7
dreamonaut <dreamonautre@gmail.com>
2023-06-19 21:27:48 +0500

    Меняет пути на пути из среды

--- packages/client/src/pages/Signin/hooks/useYandexSignIn.ts
+++ packages/client/src/pages/Signin/hooks/useYandexSignIn.ts
@@ -6,23 +6,15 @@ import {
   useSignInYandexMutation,
 } from '../../../app/store/api/auth/authApi';
 
-// Пока захардкодил чтобы можно было проверить на рендере
-// В дальнейшем будут браться с env
-// TODO localhost only for local test
-const OAUTH_REDIRECT_API_PATH =
-  'https://oauth.yandex.ru/authorize?response_type=code';
-// const APP_PATH = 'https://multiplayer-game-6e3r.onrender.com/';
-const APP_PATH = 'http://localhost:3000';
-
 const getYandexAuthRedirectLink = ({ clientId }: { clientId: string }) =>
-  `${OAUTH_REDIRECT_API_PATH}&client_id=${clientId}&redirect_uri=${APP_PATH}`;
+  `${__YANDEX_OAUTH_REDIRECT_PATH__}&client_id=${clientId}&redirect_uri=${__APP_PATH__}`;
 
 export const useYandexSignIn = () => {
   const [signInYandex, { isError }] = useSignInYandexMutation();
   const [searchParams] = useSearchParams();
 
   const { data } = useGetServiceIdQuery({
-    redirect_uri: encodeURIComponent(APP_PATH),
+    redirect_uri: encodeURIComponent(__APP_PATH__),
   });
 
   const authCode = searchParams.get('code');
@@ -31,7 +23,7 @@ export const useYandexSignIn = () => {
     if (authCode && authCode !== 'null' && !isError) {
       signInYandex({
         code: authCode,
-        redirect_uri: APP_PATH,
+        redirect_uri: __APP_PATH__,
       });
     }
   }, [authCode]);
