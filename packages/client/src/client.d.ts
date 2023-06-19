bd0eeddbf9886bedcc69bb2a2a282644f92c32b8
lusorich <44974580+lusorich@users.noreply.github.com>
2023-06-19 22:30:15 +0500

    Main into prod (#56)
    * Update AuthenticatedApp.tsx
    
    * Исправляет часть проблем при ouath
    
    * Sprint 9 (#53)
    
    * fix валидации формы паролей
    
    * Исправляет ошибки при билде
    
    * Update AuthenticatedApp.tsx
    
    * Update AuthenticatedApp.tsx
    
    * Feature/ode 66 (#42)
    
    * ODE-66: добавляет автообновление счетчика комментариев
    
    * ODE-66: дорабатывает апи форума
    
    * ODE-66: дорабатывает апи форума
    
    * Добавлен pgadmin + настройка docker-compose (#43)
    
    * Feature/ode 65 + ode-63 (#44)
    
    * Update AuthenticatedApp.tsx
    
    * Исправляет часть проблем при ouath
    
    * Feature/ode 66 (#42)
    
    * ODE-66: добавляет автообновление счетчика комментариев
    
    * ODE-66: дорабатывает апи форума
    
    * ODE-66: дорабатывает апи форума
    
    * Добавлен pgadmin + настройка docker-compose (#43)
    
    * Тест деплоя в Яндекс облако
    
    * При пуше в ветку prod + убрала лишние переменные
    
    ---------
    
    Co-authored-by: lusorich <44974580+lusorich@users.noreply.github.com>
    Co-authored-by: dreamonaut <dreamonautre@gmail.com>
    
    * Delete registry secret
    
    * Add MEMORYLEAKS
    
    * Feature/api leader b (#47)
    
    Leaderboard API Бэк + Фронт,
    Forum рендер дерева комментариев
    
    * add CSP-headers (#50)
    
    add CSP
    
    * ODE-64: делает авторизацию через бек (#46)
    
    * ODE-64: делает авторизацию через бек
    
    * ODE-64: делает авторизацию через бек
    
    * ODE-64: переносит базовые пути в енв
    
    * Убирает неиспользуемый импорт
    
    * Меняет пути на пути из среды
    
    ---------
    
    Co-authored-by: aleksakko <aleksakko@yandex.ru>
    Co-authored-by: Maria <57846361+shinonhorror@users.noreply.github.com>
    Co-authored-by: shinonhorror <shinonhorror@gmail.com>
    Co-authored-by: aleksakko <122122549+aleksakko@users.noreply.github.com>
    
    * Sprint 9 (#55)
    
    * Feature/ode 66 (#42)
    
    * ODE-66: добавляет автообновление счетчика комментариев
    
    * ODE-66: дорабатывает апи форума
    
    * ODE-66: дорабатывает апи форума
    
    * Добавлен pgadmin + настройка docker-compose (#43)
    
    * Add MEMORYLEAKS
    
    * Feature/api leader b (#47)
    
    Leaderboard API Бэк + Фронт,
    Forum рендер дерева комментариев
    
    * add CSP-headers (#50)
    
    add CSP
    
    * ODE-64: делает авторизацию через бек (#46)
    
    * ODE-64: делает авторизацию через бек
    
    * ODE-64: делает авторизацию через бек
    
    * ODE-64: переносит базовые пути в енв
    
    * Убирает неиспользуемый импорт
    
    * Меняет пути на пути из среды
    
    * Добавляет енв
    
    ---------
    
    Co-authored-by: Maria <57846361+shinonhorror@users.noreply.github.com>
    Co-authored-by: shinonhorror <shinonhorror@gmail.com>
    Co-authored-by: aleksakko <122122549+aleksakko@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: aleksakko <aleksakko@yandex.ru>
    Co-authored-by: Maria <57846361+shinonhorror@users.noreply.github.com>
    Co-authored-by: shinonhorror <shinonhorror@gmail.com>
    Co-authored-by: aleksakko <122122549+aleksakko@users.noreply.github.com>

--- packages/client/src/client.d.ts
+++ packages/client/src/client.d.ts
@@ -2,3 +2,4 @@ declare const __SERVER_PORT__: number;
 declare const __CLIENT_URL__: string;
 declare const __APP_PATH__: string;
 declare const __YANDEX_OAUTH_REDIRECT_PATH__: string;
+declare const __BASE_API_PATH__: string;
